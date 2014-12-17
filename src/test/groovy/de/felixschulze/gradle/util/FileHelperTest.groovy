00ad6f5554591f3a1d6439fbc1f637fddac625fa
Felix Schulze <code@felixschulze.de>
2014-12-17 15:51:03 +0100

    Add first tests

---
+++ src/test/groovy/de/felixschulze/gradle/util/FileHelperTest.groovy
@@ -0,0 +1,31 @@
+package de.felixschulze.gradle.util
+
+import java.util.regex.PatternSyntaxException
+
+class FileHelperTest extends GroovyTestCase {
+
+    void testNullChecks() {
+        assertNull(FileHelper.getFile(null, null))
+        assertNull(FileHelper.getFile("*", null))
+    }
+
+    void testNotExistingDirectory() {
+        assertNull(FileHelper.getFile("*", new File("u4wzteg7ufgew78w49ufzw97hezgo4ew89gwhu")))
+    }
+
+    void testWrongRegex() {
+        shouldFail(PatternSyntaxException) {
+            FileHelper.getFile("*", new File("."))
+        }
+    }
+
+    void testCorrectRegex() {
+        File file = FileHelper.getFile(".*.md", new File("."))
+        assertNotNull(file)
+        assertEquals(file.name, "README.md")
+    }
+
+    void testCorrectRegexButNoFilePresent() {
+        assertNull(FileHelper.getFile(".*.xyzxyz", new File(".")))
+    }
+}
